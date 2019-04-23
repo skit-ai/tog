7455d24a71fd17a78288f8f34d1cdca08807f2a8
Abhinav Tushar <lepisma@fastmail.com>
2019-04-24 03:22:27 +0530

    clean old test

--- test/tog-test.el
+++ test/tog-test.el
@@ -1,13 +1,6 @@
 (push default-directory load-path)
 
-(require 'cl-lib)
-(require 'ov)
 (require 'tog)
 
-(defvar test-file "./test/data/test.tog")
-
-(ert-deftest test-tag-loading ()
-  (with-current-buffer (find-file-noselect test-file)
-    (should (= (length (cl-remove-if (lambda (o) (null (ov-val o 'tog-line-id))) (ov-all)))
-               (with-current-buffer (find-file-noselect (tog-tag-file-name))
-                 (count-matches "^[0-9]+: "))))))
+(ert-deftest test-tag ()
+  (should (= 1 1)))
