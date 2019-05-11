7cc977427ee8037b08a539d166f4a0cb52af8322
Abhinav Tushar <lepisma@fastmail.com>
2019-05-11 20:22:42 +0530

    update setup for taggingc

--- init.el
+++ init.el
@@ -25,20 +25,23 @@
 ;; Cache dir for audios
 (setq tog-player-cache (expand-file-name "./audios/"))
 
-;; NOTE: Here is a pipeline for tagging entities
+;; ---------------------------------
+;; Setup for tagging ranged entities
+;; ---------------------------------
 (setq tog-types '("PEOPLE" "DATE" "TIME" "NUMBER" "DATETIME"))
 (setq tog-conv-method 'ranged)
-
 ;; First we load the data file with items to tag
-(tog-stats-count "./alts-dt-num.json")
 (tog-conv-load-from-json "./alts-dt-num.json")
 ;; Next, optionally, load the already done tags
 (tog-load-tags)
+;; Start the tagging
+(tog)
 
-;; NOTE: Here is a pipeline for single intent +/- tagging.
+;; -----------------------------------
+;; Setup for single intent +/- tagging
+;; -----------------------------------
 (setq tog-types '("LOCATION-PRESENT"))
 (setq tog-conv-method 'boolean)
-(tog-stats-count "./alts-location.json")
 (tog-conv-load-from-json "./alts-location.json")
 (tog-load-tags)
 
@@ -49,3 +52,22 @@
   (tog-conv-tag))
 
 (add-hook 'tog-conv-after-tag-hook #'tog-conv-go-go)
+(tog)
+
+;; -----------------------
+;; Setup for transcription
+;; -----------------------
+(setq tog-types '("TRANSCRIPT"))
+(setq tog-conv-method 'transcript)
+(tog-conv-load-from-json "./alts-transcript.json")
+(tog-load-tags)
+(tog)
+
+;; --------------------------
+;; Setup for single range NER
+;; --------------------------
+(setq tog-types '("LOCATION"))
+(setq tog-conv-method 'ranged)
+(tog-conv-load-from-json "./alts-location.json")
+(tog-load-tags)
+(tog)
