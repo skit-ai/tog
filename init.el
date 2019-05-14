941819c72d5aed5f01f743e40614beed8f3fc639
Abhinav Tushar <lepisma@fastmail.com>
2019-05-14 19:39:58 +0530

    move init.el to init.sample.el

--- init.el
+++ init.el
@@ -1,5 +1,3 @@
-;; Basic init file for working with standalone `cask emacs'
-
 (add-to-list 'load-path default-directory)
 (setq make-backup-files nil)
 (menu-bar-mode -1)
@@ -36,39 +34,4 @@
 ;; Next, optionally, load the already done tags
 (tog-load-tags)
 ;; Start the tagging
-(tog)
-
-;; -----------------------------------
-;; Setup for single intent +/- tagging
-;; -----------------------------------
-(setq tog-types '("LOCATION-PRESENT"))
-(setq tog-conv-method 'boolean)
-(tog-conv-load-from-json "./alts-location.json")
-(tog-load-tags)
-
-;; Hook for fast jumps
-(defun tog-conv-go-go ()
-  (tog-next)
-  (tog-conv-play)
-  (tog-conv-tag))
-
-(add-hook 'tog-conv-after-tag-hook #'tog-conv-go-go)
-(tog)
-
-;; -----------------------
-;; Setup for transcription
-;; -----------------------
-(setq tog-types '("TRANSCRIPT"))
-(setq tog-conv-method 'transcript)
-(tog-conv-load-from-json "./alts-transcript.json")
-(tog-load-tags)
-(tog)
-
-;; --------------------------
-;; Setup for single range NER
-;; --------------------------
-(setq tog-types '("LOCATION"))
-(setq tog-conv-method 'ranged)
-(tog-conv-load-from-json "./alts-location.json")
-(tog-load-tags)
 (tog)
---
+++ init.sample.el
@@ -0,0 +1,79 @@
+;; Example init file for working with standalone `cask emacs'
+
+(add-to-list 'load-path default-directory)
+
+(require 'tog)
+(require 'tog-stats)
+
+;; Define Keys
+(define-key tog-mode-map (kbd "RET") 'tog-conv-tag)
+(define-key tog-mode-map (kbd "n") 'tog-next)
+(define-key tog-mode-map (kbd "N") 'tog-next-untagged)
+(define-key tog-mode-map (kbd "p") 'tog-prev)
+(define-key tog-mode-map (kbd "P") 'tog-prev-untagged)
+(define-key tog-mode-map (kbd "SPC") 'tog-conv-play)
+
+(define-key tog-mode-map (kbd "DEL") 'tog-conv-clear)
+
+(define-key tog-mode-map (kbd "q") 'tog-quit)
+(define-key tog-mode-map (kbd "C-x C-s") 'tog-save)
+
+;; Cache dir for audios
+(setq tog-player-cache (expand-file-name "./audios/"))
+(setq tog-player-command "mplayer") ;; sox, cvlc etc.
+
+;; ---------------------------------
+;; Setup for tagging ranged entities
+;; ---------------------------------
+(setq tog-types '("PEOPLE" "DATE" "TIME" "NUMBER" "DATETIME"))
+(setq tog-conv-method 'ranged)
+;; First we load the data file with items to tag
+(tog-conv-load-from-json "./conv-dt-num.json")
+;; Next, optionally, load the already done tags
+(tog-load-tags)
+;; Start the tagging
+(tog)
+
+;; -----------------------------------
+;; Setup for single intent +/- tagging
+;; -----------------------------------
+(setq tog-types '("LOCATION-PRESENT"))
+(setq tog-conv-method 'boolean)
+(tog-conv-load-from-json "./conv-location.json")
+(tog-load-tags)
+
+;; Hook for fast jumps
+(defun tog-conv-go-go ()
+  (tog-next)
+  (tog-conv-play)
+  (tog-conv-tag))
+
+(add-hook 'tog-conv-after-tag-hook #'tog-conv-go-go)
+(tog)
+
+;; -----------------------
+;; Setup for transcription
+;; -----------------------
+(setq tog-types '("TRANSCRIPT"))
+(setq tog-conv-method 'transcript)
+(tog-conv-load-from-json "./conv-transcript.json")
+(tog-load-tags)
+(tog)
+
+;; --------------------------
+;; Setup for single range NER
+;; --------------------------
+(setq tog-types '("LOCATION"))
+(setq tog-conv-method 'ranged)
+(tog-conv-load-from-json "./conv-location.json")
+(tog-load-tags)
+(tog)
+
+;; ------------------------
+;; Setup for intent tagging
+;; ------------------------
+(setq tog-types '("some-intent" "another-intent"))
+(setq tog-conv-method 'boolean)
+(tog-conv-load-from-json "./conv-intent.json")
+(tog-load-tags)
+(tog)
